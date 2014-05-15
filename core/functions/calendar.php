29e2fee01ba6964b4a83c7412a75fe1984d93297
roeku <donmezfurkan@gmail.com>
2014-05-15 22:01:48 +0200

    colleagues werkende krijgen

--- core/functions/calendar.php
+++ core/functions/calendar.php
@@ -19,7 +19,6 @@ function getAllAppointments($day, $month, $year, $userID){
 			echo '<li><div class="time">'.timestampToHours($row['starttime']).' till '.timestampToHours($row['endtime']).'</div><div class="title">'.$row['title'].'</div><div class="location">'.$row['location'].'</div></li>';
 			}
 	}	
-	getOthersAppointments($day, $month, $year, $userID);
 }
 
 //get other peeps appointments with names
@@ -35,6 +34,30 @@ function getOthersAppointments($day, $month, $year, $userID){
 	}	
 }
 
+function getColleagues($userID){
+	$result = mysql_query("SELECT * FROM `UCPM_employees` WHERE userID!=$userID");
+	if (mysql_num_rows($result) == 0){
+		echo 'No colleagues found';
+	} else {
+		while($row = mysql_fetch_array($result)){
+			echo '<a href="c_calendar.php?day='.date("j").'&month='.date("n").'&year='.date("Y").'&id='.$row['userID'].'">';
+			echo '<article class="info_list"><div class="info_pic"><img src="img/'.$row['img'].'" height="80"></div><div class="info_container"><div class="info_title">'.$row['name'].'</div><div class="info_extra">'.$row['function'].' min</div></div></article>';
+			echo '</a>';
+			}
+	}	
+}
+
+
+function getColleaguesName($userID){
+	$result = mysql_query("SELECT name FROM `UCPM_employees` WHERE userID=$userID");
+	if (mysql_num_rows($result) == 0){
+		echo 'No name found';
+	} else {
+		while($row = mysql_fetch_array($result)){
+			echo $row['name'];
+			}
+	}	
+}
 /*
 function meetingsTogether(){
 	$result = mysql_query("SELECT * FROM `UCPM_appointments` WHERE (DATE(starttime) = '$year-$month-$day') AND inviteesID!=0")
